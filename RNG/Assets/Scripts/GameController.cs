57ca6a55a1bbadff6b78c815e72852bd3bb8b3c1
aemeadow <60167778+aemeadow@users.noreply.github.com>
2021-04-20 04:07:34 -0400

    ui die and start on special tiles

--- RNG/Assets/Scripts/GameController.cs
+++ RNG/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -16,12 +17,21 @@ public class GameController : MonoBehaviour
     public float currentYPos;
     public float nextXPos;
     public float nextYPos;
-    
+    public int status;
+
+    public GameObject die1;
+    public GameObject die2;
+    public GameObject die3;
+    public GameObject die4;
+    public GameObject die5;
+    public GameObject die6;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rB = GetComponent<Rigidbody2D>();
+        status = 0;
     }
 
     // Update is called once per frame
@@ -78,8 +88,11 @@ public class GameController : MonoBehaviour
         } 
     }
 
-    //move set distance, 1 unit per second
-    IEnumerator Movement(int distance)
+    
+
+
+        //move set distance, 1 unit per second
+        IEnumerator Movement(int distance)
     {
         while (distance > 0)
         {
@@ -112,13 +125,45 @@ public class GameController : MonoBehaviour
         {
             Debug.Log("Entered the goal");
         }
+
+        if (collision.CompareTag("respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.CompareTag ("conveyor"))
+        {
+            status = 2;
+        }
+        if (collision.CompareTag("ice"))
+        {
+            status = -1;
+        }
+        if (collision.CompareTag("onewaytrigger"))
+        {
+            //when in trigger onewaycollider, make oneway into trigger
+        }
     }
+
+    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collided with something");
+        
+        
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        status = 0;
+        if (collision.gameObject.tag == "onewaytrigger")
+        {
+            //when not in, dont
+        }
+    }
+
+
+    //THESE ARE THE BUTTONY BOYS!
     public void LeftButton()
     {
         nextXPos--;
@@ -149,11 +194,67 @@ public class GameController : MonoBehaviour
 
     public void GoButton()
     {
-        moveDistance = Random.Range(1, 6);
+        moveDistance = Random.Range(1, 6) + status;
         Debug.Log("Move distance = " + moveDistance);
         if (isMoving == false)
         {
             StartCoroutine(Movement(moveDistance));
+        }
+
+        if (moveDistance == 1)
+        {
+            die1.SetActive(true);
+            die2.SetActive(false);
+            die3.SetActive(false);
+            die4.SetActive(false);
+            die5.SetActive(false);
+            die6.SetActive(false);
+        }
+        if (moveDistance == 2)
+        {
+            die2.SetActive(true);
+            die1.SetActive(false);
+            die3.SetActive(false);
+            die4.SetActive(false);
+            die5.SetActive(false);
+            die6.SetActive(false);
+        }
+        if (moveDistance == 3)
+        {
+            die3.SetActive(true);
+            die2.SetActive(false);
+            die1.SetActive(false);
+            die4.SetActive(false);
+            die5.SetActive(false);
+            die6.SetActive(false);
+        }
+        if (moveDistance == 4)
+        {
+            die4.SetActive(true);
+            die2.SetActive(false);
+            die3.SetActive(false);
+            die1.SetActive(false);
+            die5.SetActive(false);
+            die6.SetActive(false);
+        }
+        if (moveDistance == 5)
+        {
+            die5.SetActive(true);
+            die2.SetActive(false);
+            die3.SetActive(false);
+            die4.SetActive(false);
+            die1.SetActive(false);
+            die6.SetActive(false);
+        }
+        if (moveDistance == 6)
+        {
+            die6.SetActive(true);
+            die2.SetActive(false);
+            die3.SetActive(false);
+            die4.SetActive(false);
+            die5.SetActive(false);
+            die1.SetActive(false);
+
         }
     }
 }
